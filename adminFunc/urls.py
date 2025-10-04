833005687cf3497593f552572556509a3bd4b364
HASAN-CE <hasanrangooni308@gmail.com>
2025-10-04 12:27:12 +0530

    Models Added

--- adminFunc/urls.py
+++ adminFunc/urls.py
@@ -6,5 +6,4 @@ app_name = 'adminFunc'
 urlpatterns = [
     path('login/', views.admin_login, name='admin_login'),
     path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    # path('logout/', views.admin_logout, name='logout'),
 ]
--- odooproject/settings.py
+++ odooproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +31,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = 'adminFunc.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -142,7 +144,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
